b5f3789bc95ba39aa693cba995a2734033280b05
Igor <i.monteiro10@live.com>
2024-02-15 13:54:39 -0300

    Começo dos estudos do dia 15/02

---
+++ Lógica de Programação Básica/Aulas/Aula16.py
@@ -0,0 +1,13 @@
+# # Operações condicionais em Python
+# if/elif/else
+
+entrada = input('Você deseja: (entrar/sair) ')
+
+if entrada=='entrar':
+    print('Você entrou no sistema!')
+
+elif entrada == 'sair':
+    print('Você saiu do Sistema')
+
+else:
+    print('Informação incorreta')
