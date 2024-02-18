96bfc3b4281f7414cc3ef20ba7d7218662fc9dce
Igor <i.monteiro10@live.com>
2024-02-18 07:37:52 -0300

    stdtypes

---
+++ Lógica de Programação Básica/Aulas/Aula33.py
@@ -0,0 +1,15 @@
+'''
+Stdtypes.html
+Imutáveis que vimos: str, int, float, bool
+'''
+string = 'luiz otávio'
+
+# Mudando a variavel apartir de outrav variavel a única maneira de conseguir alterar ela
+
+outra_variavel = f'{string[:3]}ABC{string[4:]}'
+
+print(outra_variavel)
+
+print(string)
+
+print(string)
