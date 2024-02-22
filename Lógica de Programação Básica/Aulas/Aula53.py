9edecc7d8bfd8cb48208a6732f6dde866a8b03e8
Igor <i.monteiro10@live.com>
2024-02-22 13:57:24 -0300

    Revendo Enumerate

--- Lógica de Programação Básica/Aulas/Aula53.py
+++ Lógica de Programação Básica/Aulas/Aula53.py
@@ -9,14 +9,19 @@ lista.append('João')
 
 lista_enumerada = enumerate(lista)
 
-lista_enumerada = list(enumerate(lista))
-
-print(lista_enumerada)
-
+#De forma geral, quando utiliza enumerate criamos uma lista encadeada aonde será apontava para um local e conseguindo ligar um ao outro!
+    # Podendo ser utilizado com for mas a melhor forma utilizando a propria biblioteca utilizando lista, aonde você percorrerá o ponteiro é apontaram ao fim para nulo!
 
 # print(next(lista_enumerada))
 
 # for item in lista_enumerada: #para cada Item q tenha Lista_enumerada
 #     print(item) #imprima
     
+
+print(lista_enumerada)
+
+
+lista_enumerada = list(enumerate(lista))
+
+print(lista_enumerada)
 
