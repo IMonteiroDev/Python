09107cff58a0e5e89a000d938547ca0129b87ecd
Igor <i.monteiro10@live.com>
2024-02-29 17:05:08 -0300

    aa

--- Python Intermediário - Funções, Dicionários, Módulos, Programação Funcional/Aulas/Aula4.py
+++ Python Intermediário - Funções, Dicionários, Módulos, Programação Funcional/Aulas/Aula4.py
@@ -1,12 +1,4 @@
-"""
-Tipos int e float:
-int -> Inteiros
-    ->Positivo ou negativo, sem sinal é considerado positivo
-
-float -> Número com pontos Flutuantes
+'''
 
 
-Type -> Mostra o tipo que o python é
-"""
-
-print(type(2.2))
+'''
