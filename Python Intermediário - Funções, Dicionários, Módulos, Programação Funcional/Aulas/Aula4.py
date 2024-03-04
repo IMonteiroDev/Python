9c2cf4485fa2fd51d86d0a29ccc90a5cdffb2116
Igor <i.monteiro10@live.com>
2024-03-04 14:11:44 -0300

    retorno semana

--- Python Intermediário - Funções, Dicionários, Módulos, Programação Funcional/Aulas/Aula4.py
+++ Python Intermediário - Funções, Dicionários, Módulos, Programação Funcional/Aulas/Aula4.py
@@ -1,4 +1,16 @@
 '''
 
+Escopo de Funções em Python
+Escopo significa o local onde aquele código pode atingir.
+Existe o escopo global e local.
+O Escopo global é o escopo onde todo o código é alcançavel.
+O Escopo Local é o escopo onde apenas nomes do mesmo local podem ser alcançados.
 
 '''
+
+def escopo():
+    x = 1
+    print(x)
+    
+
+escopo()
