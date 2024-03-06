2519d3baf27e533c77f80ad810808064af8d6d65
Igor <i.monteiro10@live.com>
2024-03-06 16:39:43 -0300

    Manipulação de Chaves

--- Python Intermediário - Funções, Dicionários, Módulos, Programação Funcional/Aulas/Aula13.py
+++ Python Intermediário - Funções, Dicionários, Módulos, Programação Funcional/Aulas/Aula13.py
@@ -1,19 +1,27 @@
-# Formatando a mensagem do jeito que eu queria utilizando o f-String
+'''
 
-nome = 'Luiz Otávio'
-altura = 1.80
-peso = 95
-imc = peso / altura ** 2
+Manipulando Chaves e valores em dicionários
 
-"f-strings"
-linha_1 = f'{nome} tem {altura:.2f} de altura,'
-linha_2 = f'pesa {peso} quilos e seu imc é'
-linha_3 = f'{imc:.2f}'
+'''
 
-print(linha_1)
-print(linha_2)
-print(linha_3)
+pessoa ={}
 
-# Luiz Otávio tem 1.80 de altura,
-# pesa 95 quilos e seu IMC é
-# 29.320987654320987
+
+chave = 'nome'
+
+pessoa[chave] = 'Igor Monteiro'
+pessoa['sobrenome'] = 'Monteiro'
+lista = []
+
+
+# del pessoa['sobrenome']
+print(pessoa)
+print(pessoa[chave])
+
+if pessoa.get('sobrenome') is None :
+    print('NÃO EXISTE')
+else:
+    print('Existe')
+    print(pessoa['sobrenome'])
+    
+# print('ISSO NÃO EXISTE')
