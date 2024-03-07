484378c5a20b2c2a44fc02ce869e873de358cf8c
Igor <i.monteiro10@live.com>
2024-03-07 15:54:09 -0300

    0703

--- Python Intermediário - Funções, Dicionários, Módulos, Programação Funcional/Aulas/Aula14.py
+++ Python Intermediário - Funções, Dicionários, Módulos, Programação Funcional/Aulas/Aula14.py
@@ -1,9 +1,40 @@
-#formatando apartir do Format
+'''
 
-a = 'A';
-b = 'B';
-c = 1.1;
+Métodos úteis do dicionários em Python
+len -> Quantas chaves
+keys -> Iteraveis com chaves
+values -> Iteravel com os valores
+items ->  Iteravel com chaves e valores
+setdefault -> adiciona valor se a chave não existe
+copy -> retorna uma cópia rasa(shallow copy)
+get -> obtém uma chave
+pop -> Apaga um item com a chave especificada
+popitem -> Apaga o último item adicionado
+update -> Atualiza um dicionário com outro
 
-formato = 'a = {} b = {} c ={:.1f} '.format(a,b,c);
+'''
 
-print(formato)
+pessoa = {
+    'nome': 'Luiz Otávio',
+    'sobrenome': 'Miranda',
+    # 'idade': 900,
+}
+
+pessoa.setdefault('idade',None) #defini caso não criado a key 
+
+print(pessoa.get('idade'))
+
+
+# print(list(pessoa.keys()))
+# print(list(pessoa.values()))
+# print(list(pessoa.items()))
+
+
+# for chave in pessoa:
+#     print(chave)
+
+# for valor in pessoa.values():
+#     print(valor)
+
+# for chave, valor in pessoa.items():
+#     print(chave, valor)
