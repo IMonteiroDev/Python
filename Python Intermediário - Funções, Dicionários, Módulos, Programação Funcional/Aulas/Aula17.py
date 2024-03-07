529161e86fa99de6d5d74237f79d4a96c9350ab6
Igor <i.monteiro10@live.com>
2024-03-07 17:46:32 -0300

    pp

--- Python Intermediário - Funções, Dicionários, Módulos, Programação Funcional/Aulas/Aula17.py
+++ Python Intermediário - Funções, Dicionários, Módulos, Programação Funcional/Aulas/Aula17.py
@@ -1,24 +1,59 @@
-# if / elif      / else
-# se / se não se / se não
+# Exercício - sistema de perguntas e respostas
+import os
 
-condicao1 = False
-condicao2 = True
-condicao3 = True
-condicao4 = True
+perguntas = [
+    {
+        'Pergunta': 'Quanto é 2+2?',
+        'Opções': ['1', '3', '4', '5'],
+        'Resposta': '4',
+    },
+    {
+        'Pergunta': 'Quanto é 5*5?',
+        'Opções': ['25', '55', '10', '51'],
+        'Resposta': '25',
+    },
+    {
+        'Pergunta': 'Quanto é 10/2?',
+        'Opções': ['4', '5', '2', '1'],
+        'Resposta': '5',
+    },
+]
 
-if condicao1:
-    print('Código para condição 1')
-    print('Código para condição 1')
-elif condicao2:
-    print('Código para condição 2')
-elif condicao3:
-    print('Código para condição 3')
-elif condicao4:
-    print('Código para condição 4')
-else:
-    print('Nenhuma condição foi satisfeita.')
+qtdAcertos = 0
 
-if 10 == 10:
-    print('Outro if')
-
-print('Fora do if')
+for quest in perguntas:
+    print(f'Pergunta: {quest['Pergunta']}')
+    print()
+    
+    opcoes = quest['Opções']
+    for i, choice in enumerate(opcoes):
+        print(f'{i})', choice)
+    print()
+    
+    escolha = input('Escolha uma opção: ')
+    
+    acertou = False
+    escolha_int = None
+    qtdOpcoes = len(opcoes)
+    
+    if escolha.isdigit():
+        escolha_int = int(escolha)
+        
+    if escolha_int is not None:
+        if escolha_int>=0 and escolha_int< qtdOpcoes:
+            if opcoes[escolha_int]== quest['Resposta']:
+                acertou = True
+    
+    print()
+    if acertou:
+        os.system('clear')
+        print('Você acertou ')
+        qtdAcertos+=1
+    else:   
+        os.system('clear')
+        print('Errou')
+        
+    print()
+    
+    
+print(f'Você acertou {qtdAcertos}')
