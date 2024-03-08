1d342bf40af16eac85c7e53eec8da64da3aea931
Igor <i.monteiro10@live.com>
2024-03-08 15:57:56 -0300

    Exercicios para correção

--- Python Intermediário - Funções, Dicionários, Módulos, Programação Funcional/Aulas/Aula18.py
+++ Python Intermediário - Funções, Dicionários, Módulos, Programação Funcional/Aulas/Aula18.py
@@ -1,5 +1,65 @@
 '''
 
+Sets - Conjuntos em Python(tipo set)
+Conjuntos são ensinados na matemática
 
+Representados graficamente pelo Diagrama de Venn
+Sets em Pythib são mutáveis, porém aceitam apenas 
+tipos imutáveis como valor interno
 
 '''
+
+#set(itéravel) ou {1,2,3}
+
+# s1 = {'Luiz',1,2,3} #set com Dados
+# s1 = set('') #set Vazio
+
+# print(s1)
+
+'''
+
+Sets são eificientes para remover valores duplicados de iteráveis.
+Não aceitam valores mutáveis;
+Seus valores serão sempre únicos;
+Não tem índexes;
+Não garantem Ordem;
+São Iteráveis (for, in ,not in)
+
+'''
+# l1 = {1,2,3,4,4,4,4,4,1}
+
+# s1 = set(l1)
+# l2 = list(s1)
+
+# s1 = {1,2,3}
+
+# print(3 not in s1) #possibilidade de iteraveis para averiguação de valores internos salvos no set!
+
+# for numero in s1:
+#     print(numero)
+
+# s1 = set()
+# s1.add('Luiz')
+# s1.add(1)
+# s1.update(('Olá Mundo',1,2,3,4)) #Aqui estamos passando tudo como iteravel
+# # s1.clear()
+# s1.discard('Olá Mundo')
+# print(s1)
+
+# Métodos Úteis:
+# add, update, clear, discard
+
+# Operadores Úteis:
+#     União | união (union) - une
+# Intersecção & (intersection) - Itens presentes em ambos
+# Diferença - Itens presentes apenas no set da esquerda
+# Diferença Simétrica ^ - Itens que não estão em ambos
+
+s1 = {1,2,3}
+s2 = {2,3,4}
+# s3 = s1|s2
+# s3 = s1 & s2
+# s3 = s1 - s2
+s3 = s1^s2
+
+print(s3)
--- Python Intermediário - Funções, Dicionários, Módulos, Programação Funcional/Aulas/Aula19.py
+++ Python Intermediário - Funções, Dicionários, Módulos, Programação Funcional/Aulas/Aula19.py
@@ -1,17 +1,17 @@
-"""
-Operadores de comparação (relacionais)
-OP      Significado         Exemplo (True)
->       maior               2 > 1
->=      maior ou igual      2 >= 2
-<       menor               1 < 2
-<=      menor ou igual      2 <= 2
-==      igual               'a' == 'a'
-!=      diferente           'a' != 'b'
-"""
-maior = 2 > 1
-maior_ou_igual = 2 >= 2
-menor = 1 < 2
-menor_ou_igual = 2 <= 2
-igual = 'a' == 'a'
-diferente = 'a' != 'b'
-print('Olha meu print aqui')
+# Exemplos de uso de sets
+
+import os
+
+
+letras = set()
+
+while True:
+    letra = input('Digite: ')
+    letras.add(letra.lower())
+    
+    if 'l' in letras:
+        os.system('clear')
+        print('Parabéns')
+        break
+    
+    print(letras) 
