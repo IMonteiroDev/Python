ec6ea066a1ec7207f1db0330f6caffcd04f9c17b
Igor <i.monteiro10@live.com>
2024-03-18 14:19:28 -0300

    Retorno pós Dengue

--- Python Intermediário - Funções, Dicionários, Módulos, Programação Funcional/Aulas/Aula23.py
+++ Python Intermediário - Funções, Dicionários, Módulos, Programação Funcional/Aulas/Aula23.py
@@ -1,9 +1,44 @@
-# Operador lógico "not"
-# Usado para inverter expressões
-# not True = False
-# not False = True
-senha = input('Senha: ')
+'''
+
+Empacotamento e desempacotamento de dicionários
+
+'''
+
+# a, b = 1,2
+# a,b = b,a
 
 
-print(not True)  # False
-print(not False)  # True
+
+# (a1, a2), (b1, b2) = pessoa.items()
+# print(a1, a2)
+# print(b1, b2)
+# print()
+
+# for chave, valor in pessoa.items():
+#     print(chave, valor)
+    
+
+pessoa ={
+    'nome': 'Aline',
+    'sobrenome': 'Souza',
+}
+
+dados_pessoa ={
+    'idade': 16,
+    'altura': 1.6,
+}
+
+pessoa_completa = {**pessoa, **dados_pessoa}
+
+# print(pessoa_completa)
+
+
+def mostro_argumentos_nomeados(*args, **kwargs):
+    print('Não nomeados:', args)
+    
+    for chave, valor in kwargs.items():
+        print(chave, valor)
+    
+# mostro_argumentos_nomeados(nome='Joana', qlq=123)
+
+mostro_argumentos_nomeados(**pessoa_completa)
