e6d06e6fa106b6a82f00e7e516fa1c108885b5a0
Igor <i.monteiro10@live.com>
2024-03-26 15:33:33 -0300

    2603

--- Python Intermediário - Funções, Dicionários, Módulos, Programação Funcional/Aulas/Aula33.py
+++ Python Intermediário - Funções, Dicionários, Módulos, Programação Funcional/Aulas/Aula33.py
@@ -1,15 +1,5 @@
 '''
-Stdtypes.html
-Imutáveis que vimos: str, int, float, bool
+
+Generator expression, Iterables e Iterators em Python
+
 '''
-string = 'luiz otávio'
-
-# Mudando a variavel apartir de outrav variavel a única maneira de conseguir alterar ela
-
-outra_variavel = f'{string[:3]}ABC{string[4:]}'
-
-print(outra_variavel)
-
-print(string)
-
-print(string)
