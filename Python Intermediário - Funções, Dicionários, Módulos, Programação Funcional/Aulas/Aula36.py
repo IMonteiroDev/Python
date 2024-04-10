a20a4db1ee40e4381c72e7d0a4ef2238f4e5e7d1
Igor <i.monteiro10@live.com>
2024-04-10 16:29:15 -0300

    text

--- Python Intermediário - Funções, Dicionários, Módulos, Programação Funcional/Aulas/Aula36.py
+++ Python Intermediário - Funções, Dicionários, Módulos, Programação Funcional/Aulas/Aula36.py
@@ -1,1 +1,24 @@
-11
+# Try, except, else e finally
+
+
+
+try:
+    a = 18
+    b = 0
+    print('Linha 1')
+    c = a/b
+    print('Linha 2')
+
+except ZeroDivisionError:
+    print('Dividiu por 0 ')
+    
+except NameError:
+    print('Nome b não está Definido')
+
+except (TypeError, IndexError):
+    print('TypeError + Index Error')
+    
+except Exception:
+    print('Erro Desconhecido')
+    
+print('Continua')
