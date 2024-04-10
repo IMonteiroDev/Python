6fef976d14d9e86a3459175f9d74c3fe405e7375
Igor <i.monteiro10@live.com>
2024-04-10 17:04:22 -0300

    test2

--- Python Intermediário - Funções, Dicionários, Módulos, Programação Funcional/Aulas/Aula36.py
+++ Python Intermediário - Funções, Dicionários, Módulos, Programação Funcional/Aulas/Aula36.py
@@ -1,10 +1,11 @@
 # Try, except, else e finally
 
-
+string = 'Luiz'
 
 try:
     a = 18
     b = 0
+    # print(b[0])
     print('Linha 1')
     c = a/b
     print('Linha 2')
@@ -15,8 +16,10 @@ except ZeroDivisionError:
 except NameError:
     print('Nome b não está Definido')
 
-except (TypeError, IndexError):
+except (TypeError, IndexError) as error:
     print('TypeError + Index Error')
+    print('MSG:', error)
+    print('Nome:', error.__class__.__name__)
     
 except Exception:
     print('Erro Desconhecido')
