4f0f5ecb12043c3856744b9bdd5e89cdfb731969
Igor <i.monteiro10@live.com>
2024-04-15 18:12:35 -0300

    Utilização de Closure, Muito interessante no caso

--- Python Intermediário - Funções, Dicionários, Módulos, Programação Funcional/Aulas/Aula44.py
+++ Python Intermediário - Funções, Dicionários, Módulos, Programação Funcional/Aulas/Aula44.py
@@ -1,4 +1,5 @@
 #Exercicios - Adiando Execução de Funções
+# Utiliza o Yield maybe
 
 def soma(x, y):
     return x+y
@@ -6,8 +7,15 @@ def soma(x, y):
 def multiplica(x,y):
     return x*y
 
-def criar_funcao(funcao, *args):
-    return funcao(*args)
+
+# Utilizado aqui Closure para ter uma recursão interna e adiar o processo
+def criar_funcao(funcao, x):
+    def adiar(y):
+        return funcao(x,y)
+    return adiar
 
 soma_com_cinco = criar_funcao(soma, 5)
 multiplica_por_dez = criar_funcao(multiplica, 10)
+
+print(soma_com_cinco(3))
+print(multiplica_por_dez(2))
